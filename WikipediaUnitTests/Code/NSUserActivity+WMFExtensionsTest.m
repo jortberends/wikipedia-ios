a5738838ca10ec09190e64c1dd2244fbe19b8003
Jort Berends <mail@jortberends.nl>
2023-03-15 16:18:46 +0100

    Added WMFExtensionsTest for places

--- WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
+++ WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
@@ -1,6 +1,7 @@
 #import <XCTest/XCTest.h>
 #import "NSUserActivity+WMFExtensions.h"
 
+@import CoreLocation;
 
 @interface NSUserActivity_WMFExtensions_wmf_activityForWikipediaScheme_Test : XCTestCase
 @end
@@ -50,6 +51,26 @@
     XCTAssertEqual(activity.wmf_type, WMFUserActivityTypeLink);
     XCTAssertEqualObjects(activity.webpageURL.absoluteString,
                           @"https://en.wikipedia.org/w/index.php?search=dog&title=Special:Search&fulltext=1");
+}
+
+- (void)testPlacesArticleURL {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?WMFArticleURL=https://en.m.wikipedia.org/wiki/Union_Square,_San_Francisco"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertEqualObjects(activity.webpageURL.absoluteString, @"https://en.m.wikipedia.org/wiki/Union_Square,_San_Francisco");
+}
+
+- (void)testPlacesCoordinateURL {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?lat=52.3547498&long=4.8339215&title=Amsterdam"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertEqualObjects(activity.wmf_placesTitle, @"Amsterdam");
+    
+    CLLocation *location = activity.wmf_placesLocation;
+    double latitude = 52.3547498;
+    double longitude = 4.8339215;
+    XCTAssertEqual(location.coordinate.latitude, latitude);
+    XCTAssertEqual(location.coordinate.longitude, longitude);
 }
 
 @end
